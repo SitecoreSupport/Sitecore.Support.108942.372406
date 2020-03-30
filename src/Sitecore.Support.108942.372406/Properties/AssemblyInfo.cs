27c00696886bad9cf0f3cfc536edd15750a72f09
eugenetkachenko <etk@sitecore.net>
2020-03-30 20:53:45 +0800

    Combination of 372406, 108942 patches

---
+++ src/Sitecore.Support.108942.372406/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.108942.372406")]
+[assembly: AssemblyProduct("Sitecore.Support.108942.372406")]
+[assembly: ComVisible(false)]
